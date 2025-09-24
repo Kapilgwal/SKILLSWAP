6cb3b6fc87fa2b86c6b253441c98c5b92dc416a9
kapil <2909kapil2001@gmail.com>
2025-09-24 13:21:26 +0530

    just create the registration field

--- swap/users/models.py
+++ swap/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Registration(models.Model):
+    username = models.CharField(max_length=1000,null=False,blank=False,unique=True)
+    name = models.CharField(max_length=1000)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=1000)
+    
+    def __str__(self):
+        return self.username
